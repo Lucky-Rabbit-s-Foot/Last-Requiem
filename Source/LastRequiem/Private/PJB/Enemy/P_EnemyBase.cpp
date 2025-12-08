3c862889f9b21cd4fb4d3d936851ed8c797a9420
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-08 10:28:57 +0900

    Ini: 기본 적 클래스 생성

---
+++ Source/LastRequiem/Private/PJB/Enemy/P_EnemyBase.cpp
@@ -0,0 +1,26 @@
+﻿#include "PJB/Enemy/P_EnemyBase.h"
+
+AP_EnemyBase::AP_EnemyBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AP_EnemyBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AP_EnemyBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AP_EnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
