9bf61ffa4ccba24e9e68535261d88df78895210c
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-09 11:06:28 +0900

    Feat: 전투 컴포넌트 추가

---
+++ Source/LastRequiem/Private/PJB/Component/P_CombatComponent.cpp
@@ -0,0 +1,27 @@
+﻿#include "PJB/Component/P_CombatComponent.h"
+
+UP_CombatComponent::UP_CombatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UP_CombatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+void UP_CombatComponent::TakeDamage ( float InDamage )
+{
+	
+}
+
+void UP_CombatComponent::OnAttack ( AActor* Target )
+{
+}
+
+void UP_CombatComponent::OnDie ()
+{
+}
--- Source/LastRequiem/Private/PJB/Enemy/P_EnemyBase.cpp
+++ Source/LastRequiem/Private/PJB/Enemy/P_EnemyBase.cpp
@@ -1,15 +1,19 @@
 ﻿#include "PJB/Enemy/P_EnemyBase.h"
 
+#include "PJB/Component/P_CombatComponent.h"
+
 AP_EnemyBase::AP_EnemyBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	CombatComp = CreateDefaultSubobject<UP_CombatComponent> ( TEXT ( "Combat Component" ) );
 
 }
 
 void AP_EnemyBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 void AP_EnemyBase::Tick(float DeltaTime)
@@ -22,5 +26,14 @@ void AP_EnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AP_EnemyBase::OnTakeDamage ( AActor* DamagedActor , float Damage , const UDamageType* DamageType , AController* InstigateBy , AActor* DamageCauser )
+{
+	
+}
+
+void AP_EnemyBase::OnDie ()
+{
 }
 
