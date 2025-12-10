ef30671c293d25b69c87a530fd475d7fa29e09d1
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-10 15:10:35 +0900

    Feat: 적 스폰 추가

---
+++ Source/LastRequiem/Private/PJB/Spawner/P_EnemySpawner.cpp
@@ -0,0 +1,94 @@
+﻿#include "PJB/Spawner/P_EnemySpawner.h"
+
+#include "Components/ArrowComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "PJB/Data/P_DataTableRows.h"
+
+
+AP_EnemySpawner::AP_EnemySpawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	ArrowComp = CreateDefaultSubobject<UArrowComponent> ( TEXT ( "ArrowComponent" ) );
+	SetRootComponent ( ArrowComp );
+	ArrowComp->ArrowColor = FColor::Red;
+	ArrowComp->ArrowSize = 2.0f;
+	
+}
+
+void AP_EnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (SpawnInterval > 0.0f && EnemyDataTable)
+	{
+		GetWorldTimerManager ().SetTimer ( 
+			SpawnTimerHandlde , 
+			this , 
+			&AP_EnemySpawner::SpawnEnemy , 
+			SpawnInterval , 
+			true , 
+			SpawnDelay 
+		);
+	}
+
+}
+
+
+void AP_EnemySpawner::SpawnEnemy ()
+{
+	if (!EnemyDataTable)
+	{
+		UE_LOG ( LogTemp , Warning , TEXT ( "EnemyDataTable is null in EnemySpawner: %s" ) , *GetName () );
+		return;
+	}
+
+	if(!EnemyTagToSpawn.IsValid())
+	{
+		UE_LOG ( LogTemp , Warning , TEXT ( "EnemyTagToSpawn is invalid in EnemySpawner: %s" ) , *GetName () );
+		return;
+	}
+
+	FEnemySpawnRow* SelectedRow = nullptr;
+
+	static const FString ContextString ( TEXT ( "EnemySpawnContext" ) );
+	TArray<FEnemySpawnRow*> AllRows;
+	EnemyDataTable->GetAllRows<FEnemySpawnRow> ( ContextString , AllRows );
+
+	for(FEnemySpawnRow* Row : AllRows)
+	{
+		if (Row && Row->EnemyTag.MatchesTag ( EnemyTagToSpawn ))
+		{
+			SelectedRow = Row;
+			break;
+		}
+	}
+
+	if (!SelectedRow || !SelectedRow->EnemyClass)
+	{
+		UE_LOG ( LogTemp , Warning , TEXT ( "No matching enemy found for tag in EnemySpawner: %s" ) , *GetName () );
+		return;
+	}
+
+	FTransform SpawnTransform = GetActorTransform ();
+	
+	APawn* SpawnedEnemy = GetWorld ()->SpawnActorDeferred<APawn> (
+		SelectedRow->EnemyClass , 
+		SpawnTransform , 
+		nullptr , 
+		nullptr , 
+		ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn 
+	);
+
+	if (SpawnedEnemy)
+	{
+		// initialize any propertiese on enemy
+
+		UGameplayStatics::FinishSpawningActor ( SpawnedEnemy , SpawnTransform );
+		if (SpawnedEnemy->GetController() == nullptr)
+		{
+			SpawnedEnemy->SpawnDefaultController ();
+		}
+	}
+}
---
+++ Source/LastRequiem/Public/PJB/Spawner/P_EnemySpawner.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataTable.h"
+
+#include "P_EnemySpawner.generated.h"
+
+UCLASS()
+class LASTREQUIEM_API AP_EnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AP_EnemySpawner();
+
+	UFUNCTION ( BlueprintCallable , Category = "Factory" )
+	void SpawnEnemy ();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly , Category = "Data|SpawnSettings" )
+	FGameplayTag EnemyTagToSpawn;
+
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly , Category = "Data|SpawnSettings" )
+	UDataTable* EnemyDataTable;
+
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly , Category = "Data|SpawnSettings" )
+	float SpawnDelay = 1.0f;
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly , Category = "Data|SpawnSettings" )
+	float SpawnInterval = 5.0f;
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly , Category = "Data|SpawnSettings" )
+	float SpawnRadius = 20.0f;
+
+	FTimerHandle SpawnTimerHandlde;
+
+private:
+	UPROPERTY ( VisibleAnywhere , Category = "Visual" )
+	class UArrowComponent* ArrowComp = nullptr;
+
+};
