56f64d07d4cf814c37a650f7f6c8f94bd4f3e78b
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2025-12-10 18:17:49 +0900

    Test : SituationMapWidget 명령 버튼 테스트 함수 추가

--- Source/LastRequiem/Private/KWB/UI/Monitor/W_SituationMapWidget.cpp
+++ Source/LastRequiem/Private/KWB/UI/Monitor/W_SituationMapWidget.cpp
@@ -2,4 +2,19 @@
 
 
 #include "KWB/UI/Monitor/W_SituationMapWidget.h"
+#include "Components/Button.h"
 
+void UW_SituationMapWidget::NativeConstruct()
+{
+	Super::NativeConstruct ();
+
+	if (Exit)
+	{
+		// TEST : 실 코드에서는 Exit에는 연결 X
+		Exit->OnClicked.AddDynamic ( this , &UW_SituationMapWidget::OnMyButtonClicked );
+	}
+}
+
+void UW_SituationMapWidget::OnMyButtonClicked ()
+{
+}
