22202e0210f406daca5ca071ffe8a59bd41ddb4b
eyeofsol <101052774+jbaem@users.noreply.github.com>
2025-12-12 09:54:40 +0900

    Feat: 로그 매크로 추가
    - LastRequiem 헤더 파일 추가 후 사용 가능
    - 로그 찍는 위치 확인 가능
    - 빌드할 때 자동으로 제외

--- Source/LastRequiem/Private/LastRequiem.cpp
+++ Source/LastRequiem/Private/LastRequiem.cpp
@@ -1,6 +1,8 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LastRequiem.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LastRequiem, "LastRequiem" );
+
+DEFINE_LOG_CATEGORY ( LogLR );
