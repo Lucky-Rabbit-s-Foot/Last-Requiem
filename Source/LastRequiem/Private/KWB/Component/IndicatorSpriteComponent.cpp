b6f97424b7fd690ac297285e54a3d5e3e53baca8
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2025-12-12 15:37:54 +0900

    Chore : 이름 변경 BaseMapPaperSpriteComponent -> IndicatorSpriteComponent

---
+++ Source/LastRequiem/Private/KWB/Component/IndicatorSpriteComponent.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KWB/Component/IndicatorSpriteComponent.h"
+
+UIndicatorSpriteComponent::UIndicatorSpriteComponent ()
+{
+	// 타이머로 해결할 수 있으면 끄기
+	PrimaryComponentTick.bCanEverTick = true;
+
+	if (SpirteNormal)
+	{
+		SourceSprite = SpirteNormal;
+	}
+	else
+	{
+		UE_LOG (LogTemp, Warning, TEXT("기본 SpriteNormal 데이터가 존재하지 않습니다!"));
+	}
+
+	GlowSpeed = 5.0f;
+	GlowMin = 0.5f;
+	GlowMax = 2.0f;
+
+	bGlowActive = false;
+	GlowTime = 0.0f;
+
+	// 충돌, 그림자, 정렬 등 기본값
+	SetCollisionEnabled ( ECollisionEnabled::NoCollision );
+	SetCastShadow ( false );
+	SetReceivesDecals ( false );
+
+	// 미니맵에서만 보이게
+	bVisibleInSceneCaptureOnly = true;			 // SceneCapture에서만 표시 
+	bHiddenInGame = false;						 // 게임에서 숨기지 않는다 (SceneCapture 포함) : TEST - 테스트 후 필요시 세팅
+	SetVisibility ( true , true );
+
+	// 투명 객체 정렬 우선순위 올리기 : 테스트 후 필요할 시 세팅
+	// TranslucencySortPriority = 10;
+}
+
+void UIndicatorSpriteComponent::BeginPlay ()
+{
+
+}
+
+void UIndicatorSpriteComponent::TickComponent ( float DeltaTime , ELevelTick TickType , FActorComponentTickFunction* ThisTickFunction )
+{
+
+}
+
+void UIndicatorSpriteComponent::UpdateSpriteForState ()
+{
+}
+
+void UIndicatorSpriteComponent::UpdateGlow ( float DeltaTime )
+{
+}
---
+++ Source/LastRequiem/Public/KWB/Component/IndicatorSpriteComponent.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteComponent.h"
+#include "IndicatorSpriteComponent.generated.h"
+
+class UPaperSprite;
+class UMaterialInstanceDynamic;
+
+/**
+ * 
+ */
+UCLASS( Blueprintable , ClassGroup = (Custom) , meta = (BlueprintSpawnableComponent) )
+class LASTREQUIEM_API UIndicatorSpriteComponent : public UPaperSpriteComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UIndicatorSpriteComponent ();
+
+	virtual void BeginPlay () override;
+	virtual void TickComponent ( float DeltaTime , ELevelTick TickType , FActorComponentTickFunction* ThisTickFunction ) override;
+
+protected:
+	UPROPERTY ( EditAnywhere , Category = "Data|Spirte" )
+	TObjectPtr<UPaperSprite> SpirteNormal = nullptr;
+
+	UPROPERTY ( EditAnywhere , Category = "Data|Spirte" )
+	TObjectPtr<UPaperSprite> SpirteCombat = nullptr;
+
+	UPROPERTY ( EditAnywhere , Category = "Data|Spirte" )
+	TObjectPtr<UPaperSprite> SpirteDead = nullptr;
+
+	// Glow 세팅
+	UPROPERTY ( EditAnywhere , Category = "Data|Glow" )
+	float GlowSpeed;
+
+	UPROPERTY ( EditAnywhere , Category = "Data|Glow" )
+	float GlowMin;
+
+	UPROPERTY ( EditAnywhere , Category = "Data|Glow" )
+	float GlowMax;
+
+private:
+	// Glow용
+	UPROPERTY ( Transient )
+	UMaterialInstanceDynamic* DynamicMID;
+
+	bool  bGlowActive;
+	float GlowTime;
+
+	void UpdateSpriteForState ();
+	void UpdateGlow ( float DeltaTime );
+
+	FTimerHandle GlowTimer;	// Unit이 적 발견 시 반짝이는 효과 일정 시간 유지용
+};
