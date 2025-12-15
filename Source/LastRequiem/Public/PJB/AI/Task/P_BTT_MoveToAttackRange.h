a61238b10554b5899bd28284f4fbc858b07e95ca
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-15 17:57:05 +0900

    Feat: 적 이동 로직 수정
    - 겹쳐지지 않게 수정 중

--- Source/LastRequiem/Public/PJB/AI/Task/P_BTT_MoveToAttackRange.h
+++ Source/LastRequiem/Public/PJB/AI/Task/P_BTT_MoveToAttackRange.h
@@ -1,12 +1,12 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
-#include "BehaviorTree/Tasks/BTTask_MoveTo.h"
+#include "BehaviorTree/BTTaskNode.h"
 
 #include "P_BTT_MoveToAttackRange.generated.h"
 
 UCLASS()
-class LASTREQUIEM_API UP_BTT_MoveToAttackRange : public UBTTask_MoveTo
+class LASTREQUIEM_API UP_BTT_MoveToAttackRange : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
@@ -14,4 +14,12 @@ public:
 	UP_BTT_MoveToAttackRange ();
 
 	virtual EBTNodeResult::Type ExecuteTask ( UBehaviorTreeComponent& OwnerComp , uint8* NodeMemory ) override;
+	virtual void TickTask ( UBehaviorTreeComponent& OwnerComp , uint8* NodeMemory , float DeltaSeconds ) override;
+	virtual void OnTaskFinished ( UBehaviorTreeComponent& OwnerComp , uint8* NodeMemory , EBTNodeResult::Type TaskResult ) override;
+protected:
+	UPROPERTY ( EditAnywhere , Category = "Data|BlackBoard" )
+	struct FBlackboardKeySelector TargetActorKey;
+
+	UPROPERTY ( EditAnywhere , BlueprintReadWrite , Category = "Data|AI|Attack" )
+	float RangeRatio = 0.8f;
 };
