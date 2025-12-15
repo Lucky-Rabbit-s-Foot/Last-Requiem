91102e642b85b0dfca8927a677d008e900094f9f
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-15 15:47:21 +0900

    Feat: 적 종류 데이터 에셋으로 관리 및 적용

--- Source/LastRequiem/Private/PJB/Enemy/P_EnemyBase.cpp
+++ Source/LastRequiem/Private/PJB/Enemy/P_EnemyBase.cpp
@@ -5,6 +5,7 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "PJB/AI/P_AIControllerEnemyBase.h"
 #include "PJB/Component/P_CombatComponent.h"
+#include "PJB/Data/P_EnemyDataAsset.h"
 #include "KWB/Component/IndicatorSpriteComponent.h"
 
 #include "Navigation/PathFollowingComponent.h"
@@ -14,29 +15,19 @@ AP_EnemyBase::AP_EnemyBase()
 	PrimaryActorTick.bCanEverTick = true;
 
 	CombatComp = CreateDefaultSubobject<UP_CombatComponent> ( TEXT ( "Combat Component" ) );
-	SpriteComp = CreateDefaultSubobject<UIndicatorSpriteComponent> ( TEXT ( "Sprite Component" ) );
+	SpriteComp = CreateDefaultSubobject<UIndicatorSpriteComponent> ( TEXT ( "Indicator Sprite Component" ) );
+	
 	InitSpriteComponent ();
-
-	static FGameplayTag EnemyTag = FGameplayTag::RequestGameplayTag ( FName ( "Enemy" ) );
-	GameplayTags.AddTag ( EnemyTag );
+	InitGameplayTag ();
+	InitRotationSetting ();
 
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 }
 
-void AP_EnemyBase::InitSpriteComponent ()
-{
-	SpriteComp->SetupAttachment ( GetRootComponent() );
-	SpriteComp->SetRelativeLocation ( FVector ( 0.0f , 0.0f , 300.0f ) );
-	SpriteComp->SetRelativeRotation ( FRotator ( -90.0f , 0.0f , 0.0f ) );
-	SpriteComp->SetCastShadow ( false );
-	SpriteComp->SetCollisionEnabled ( ECollisionEnabled::NoCollision );
-}
 
 void AP_EnemyBase::BeginPlay()
 {
 	Super::BeginPlay();
-
-	InitAnimInstance ();
 
 	OnTakeAnyDamage.AddDynamic ( this , &AP_EnemyBase::OnTakeDamage );
 }
@@ -48,37 +39,59 @@ void AP_EnemyBase::Tick(float DeltaTime)
 	AIPathVisualization ();
 }
 
-void AP_EnemyBase::AIPathVisualization ()
+void AP_EnemyBase::InitSpriteComponent ()
 {
-	// AI 경로 시각화
-	AAIController* AIC = Cast<AAIController> ( GetController () );
-	if (AIC->GetPathFollowingComponent () && AIC->GetPathFollowingComponent ()->GetPath ().IsValid ())
-	{
-		const TArray<FNavPathPoint>& PathPoints = AIC->GetPathFollowingComponent ()->GetPath ()->GetPathPoints ();
-
-		for (int32 i = 0; i < PathPoints.Num () - 1; ++i)
-		{
-			DrawDebugLine (
-				GetWorld () ,
-				PathPoints[i].Location ,
-				PathPoints[i + 1].Location ,
-				FColor::Green ,
-				false , -1.0f , 0 , 3.0f
-			);
-		}
-	}
-	// -----
+	SpriteComp->SetupAttachment ( GetRootComponent () );
+	SpriteComp->SetRelativeLocation ( FVector ( 0.0f , 0.0f , 300.0f ) );
+	SpriteComp->SetRelativeRotation ( FRotator ( -90.0f , 0.0f , 0.0f ) );
+	SpriteComp->SetCastShadow ( false );
+	SpriteComp->SetCollisionEnabled ( ECollisionEnabled::NoCollision );
 }
 
-void AP_EnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AP_EnemyBase::InitGameplayTag ()
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	static FGameplayTag EnemyTag = FGameplayTag::RequestGameplayTag ( FName ( "Enemy" ) );
+	GameplayTags.AddTag ( EnemyTag );
 }
+
+void AP_EnemyBase::InitRotationSetting ()
+{
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement ()->bOrientRotationToMovement = true;
+	GetCharacterMovement ()->RotationRate = FRotator ( 0.0f , 640.0f , 0.0f );
+}
+
 
 void AP_EnemyBase::GetOwnedGameplayTags ( FGameplayTagContainer& TagContainer ) const
 {
 	TagContainer = GameplayTags;
+}
+
+void AP_EnemyBase::InitEnemyData(UP_EnemyDataAsset* InData)
+{
+	if (!InData) return;
+
+	if(InData->SkeletalMesh)
+	{
+		GetMesh()->SetSkeletalMesh ( InData->SkeletalMesh );
+	}
+
+	if(InData->AnimInstanceClass)
+	{
+		GetMesh()->SetAnimInstanceClass ( InData->AnimInstanceClass );
+	}
+		
+	GetCharacterMovement ()->MaxWalkSpeed = InData->BaseSpeed;
+
+	CachedAttackRange = InData->AttackRange;
+	CachedAttackMontage = InData->AttackMontage;
+
+	if (CombatComp)
+	{
+		CombatComp->InitStats ( InData->MaxHealth , InData->AttackRange , InData->AttackPower );
+	}
 }
 
 void AP_EnemyBase::OnTakeDamage ( AActor* DamagedActor , float Damage , const UDamageType* DamageType , AController* InstigateBy , AActor* DamageCauser )
@@ -134,10 +147,22 @@ void AP_EnemyBase::OnDeactivate()
 	SetLifeSpan ( 5.0f );
 }
 
-void AP_EnemyBase::InitAnimInstance()
+void AP_EnemyBase::AIPathVisualization ()
 {
-	if (GetMesh())
+	AAIController* AIC = Cast<AAIController> ( GetController () );
+	if (AIC->GetPathFollowingComponent () && AIC->GetPathFollowingComponent ()->GetPath ().IsValid ())
 	{
-		AnimInstance = GetMesh()->GetAnimInstance();
+		const TArray<FNavPathPoint>& PathPoints = AIC->GetPathFollowingComponent ()->GetPath ()->GetPathPoints ();
+
+		for (int32 i = 0; i < PathPoints.Num () - 1; ++i)
+		{
+			DrawDebugLine (
+				GetWorld () ,
+				PathPoints[i].Location ,
+				PathPoints[i + 1].Location ,
+				FColor::Green ,
+				false , -1.0f , 0 , 3.0f
+			);
+		}
 	}
 }
--- Source/LastRequiem/Public/PJB/Data/P_DataTableRows.h
+++ Source/LastRequiem/Public/PJB/Data/P_DataTableRows.h
@@ -3,15 +3,21 @@
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
 #include "GameplayTagContainer.h"
+#include "PJB/Data/P_EnemyDataAsset.h"
+
 #include "P_DataTableRows.generated.h"
 
 USTRUCT ( BlueprintType )
 struct FP_EnemySpawnRow : public FTableRowBase
 {
 	GENERATED_BODY ()
+
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FGameplayTag EnemyTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UP_EnemyDataAsset* EnemyDataAsset;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSubclassOf<APawn> EnemyClass;
