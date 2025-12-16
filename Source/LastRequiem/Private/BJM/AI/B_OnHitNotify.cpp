7d6e59bcb76d45edd90e36980a62cbbb9db8f54b
jackbongmin <rrrgk12@gmail.com>
2025-12-16 10:21:39 +0900

    Feat: Unit Attack Montage 적용

---
+++ Source/LastRequiem/Private/BJM/AI/B_OnHitNotify.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BJM/AI/B_OnHitNotify.h"
+#include "BJM/Unit/B_UnitBase.h"
+
+
+UB_OnHitNotify::UB_OnHitNotify ()
+{
+	NotifyColor = FColor::Red;
+}
+
+void UB_OnHitNotify::Notify ( USkeletalMeshComponent* MeshComp , UAnimSequenceBase* Animation , const FAnimNotifyEventReference& EventReference )
+{
+	Super::Notify ( MeshComp , Animation , EventReference );
+
+	if (MeshComp && MeshComp->GetOwner ())
+	{
+		AB_UnitBase* Unit = Cast<AB_UnitBase> ( MeshComp->GetOwner () );
+
+		if (Unit && Unit->IsAlive ())
+		{
+			Unit->OnAttackHit_Unit (); 
+		}
+	}
+}
