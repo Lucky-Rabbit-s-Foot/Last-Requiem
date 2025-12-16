99cc9d19104e641c4afcb94bf928a6af314704f4
jackbongmin <rrrgk12@gmail.com>
2025-12-16 15:26:45 +0900

    Feat: 범위공격 Unit class 생성

---
+++ Source/LastRequiem/Public/BJM/Unit/B_UnitGrenadier.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BJM/Unit/B_UnitBase.h"
+#include "B_UnitGrenadier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTREQUIEM_API AB_UnitGrenadier : public AB_UnitBase
+{
+	GENERATED_BODY()
+	
+};
