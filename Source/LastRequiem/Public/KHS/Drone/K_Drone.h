f8d22c3ec3c28e4d2267f492c9cf248e2be70010
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2025-12-23 14:25:55 +0900

    Feat : K_Drone 스킬 쿨 타임 추가

--- Source/LastRequiem/Public/KHS/Drone/K_Drone.h
+++ Source/LastRequiem/Public/KHS/Drone/K_Drone.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -42,6 +42,14 @@ private:
 	void InitializeDetectedUnitSlot(); //게임 시작시 맵 전체 유닛 스캔 헬퍼 함수
 	void UpdateDetectedUnitSlot(); //게임 진행 중 유닛 탐지 업데이트 헬퍼 함수
 	
+	// (20251223) W : 드론 스킬 쿨타임 핸들 용도 멤버 추가 (Start)
+	void StartSkill01Cooldown ();
+	void StartSkill02Cooldown ();
+	void OnSkill01CooldownFinished ();
+	void OnSkill02CooldownFinished ();
+	// (20251223) W : 드론 스킬 쿨타임 핸들 용도 멤버 추가 (End)
+
+
 public:
 	//Movement
 	void Move(const FVector2D& inputValue);
@@ -51,7 +59,30 @@ public:
 	void UseSkill01();
 	void UseSkill02();
 
-	
+	// (20251223) W : 드론 스킬 쿨타임 핸들 용도 함수 추가 (Start)
+	// Cooldown Query (UMG에서 프로그레스바 바인딩용)
+	UFUNCTION ( BlueprintPure , Category = "LR|Skill|Cooldown" )
+	bool IsSkill01Ready () const;
+
+	UFUNCTION ( BlueprintPure , Category = "LR|Skill|Cooldown" )
+	float GetSkill01CooldownRemaining () const;
+
+	// 0~1 (1=쿨다운 시작 직후, 0=사용 가능)
+	UFUNCTION ( BlueprintPure , Category = "LR|Skill|Cooldown" )
+	float GetSkill01CooldownRatioRemaining () const;
+
+	UFUNCTION ( BlueprintPure , Category = "LR|Skill|Cooldown" )
+	bool IsSkill02Ready () const;
+
+	UFUNCTION ( BlueprintPure , Category = "LR|Skill|Cooldown" )
+	float GetSkill02CooldownRemaining () const;
+
+	// 0~1 (1=쿨다운 시작 직후, 0=사용 가능)
+	UFUNCTION ( BlueprintPure , Category = "LR|Skill|Cooldown" )
+	float GetSkill02CooldownRatioRemaining () const;
+	// (20251223) W : 드론 스킬 쿨타임 핸들 용도 함수 추가 (End)
+
+
 private:
 	//movement 
 	FVector2D moveInputValue = FVector2D::ZeroVector;
@@ -62,6 +93,18 @@ private:
 	FTimerHandle detectionTimerHandle; 
 	TSet<AActor*> previouslyDetectedUnits; //Detected Unit Set
 	TSet<AActor*> previouslyDetectedEnemys;
+
+	// (20251223) W : 드론 스킬 쿨타임 핸들 용도 멤버 추가 (Start)
+	// 스킬 쿨타임 : 테스트 후 값 결정 필요
+	UPROPERTY ( EditDefaultsOnly , Category = "LR|Skill|Cooldown" , meta = (ClampMin = "0.0") )
+	float Skill01CooldownDuration = 5.0f;
+
+	UPROPERTY ( EditDefaultsOnly , Category = "LR|Skill|Cooldown" , meta = (ClampMin = "0.0") )
+	float Skill02CooldownDuration = 5.0f;
+
+	FTimerHandle Skill01CooldownTimerHandle;
+	FTimerHandle Skill02CooldownTimerHandle;
+	// (20251223) W : 드론 스킬 쿨타임 핸들 용도 멤버 추가 (End)
 
 protected:
 	//Components
