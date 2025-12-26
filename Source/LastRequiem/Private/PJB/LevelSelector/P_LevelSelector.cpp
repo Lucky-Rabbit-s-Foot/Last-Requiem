db024dc201d3cadc661660608aba0faf0183f988
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-26 11:18:05 +0900

    Feat: 게임 오버 버튼 UI 수정
    - 소스코드로 제어 및 상속
    - 웨이브 시간 표시 폰트 크기 축소

---
+++ Source/LastRequiem/Private/PJB/LevelSelector/P_LevelSelector.cpp
@@ -0,0 +1,59 @@
+﻿#include "PJB/LevelSelector/P_LevelSelector.h"
+
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+void UP_LevelSelector::NativeConstruct ()
+{
+	Super::NativeConstruct ();
+	if (Button1)
+	{
+		Button1->OnClicked.RemoveDynamic ( this , &UP_LevelSelector::OnButton1Clicked );
+		Button1->OnClicked.AddDynamic ( this , &UP_LevelSelector::OnButton1Clicked );
+	}
+
+	if (Button2)
+	{
+		Button2->OnClicked.RemoveDynamic ( this , &UP_LevelSelector::OnButton2Clicked );
+		Button2->OnClicked.AddDynamic ( this , &UP_LevelSelector::OnButton2Clicked );
+	}
+
+	if (Button3)
+	{
+		Button3->OnClicked.RemoveDynamic ( this , &UP_LevelSelector::OnButton3Clicked );
+		Button3->OnClicked.AddDynamic ( this , &UP_LevelSelector::OnButton3Clicked );
+	}
+}
+
+void UP_LevelSelector::OnButton1Clicked ()
+{
+	if (IsQuitGame ( Level1Name ) || Level1Name.IsNone ()) return;
+	OpenSelectedLevel ( Level1Name );
+}
+
+void UP_LevelSelector::OnButton2Clicked ()
+{
+	if (IsQuitGame ( Level2Name ) || Level2Name.IsNone ()) return;
+	OpenSelectedLevel ( Level2Name );
+}
+
+void UP_LevelSelector::OnButton3Clicked ()
+{
+	if (IsQuitGame ( Level3Name ) || Level3Name.IsNone ()) return;
+	UGameplayStatics::OpenLevel ( this , Level3Name );
+}
+
+bool UP_LevelSelector::IsQuitGame ( FName LevelName )
+{
+	if (LevelName.IsEqual ( FName ( "Quit" ) , ENameCase::IgnoreCase ))
+	{
+		UKismetSystemLibrary::QuitGame ( GetWorld () , GetWorld ()->GetFirstPlayerController () , EQuitPreference::Quit , true );
+		return true;
+	}
+	return false;
+}
+
+void UP_LevelSelector::OpenSelectedLevel ( FName LevelName )
+{
+	UGameplayStatics::OpenLevel ( this , LevelName );
+}
---
+++ Source/LastRequiem/Public/PJB/LevelSelector/P_LevelSelector.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "KHS/UI/K_BaseUIWidget.h"
+
+#include "P_LevelSelector.generated.h"
+
+class UButton;
+
+UCLASS()
+class LASTREQUIEM_API UP_LevelSelector : public UK_BaseUIWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct () override;
+
+public:
+	UPROPERTY ( meta = (BindWidget) )
+	UButton* Button1;
+
+	UPROPERTY ( meta = (BindWidget) )
+	UButton* Button2;
+
+	UPROPERTY ( meta = (BindWidget) )
+	UButton* Button3;
+
+	UPROPERTY ( EditAnywhere , BlueprintReadWrite , Category = "Data|Level" )
+	FName Level1Name = "Map_Level01";
+
+	UPROPERTY ( EditAnywhere , BlueprintReadWrite , Category = "Data|Level" )
+	FName Level2Name = "Map_Level02";
+
+	UPROPERTY ( EditAnywhere , BlueprintReadWrite , Category = "Data|Level" )
+	FName Level3Name = "Map_Level03";
+
+protected:
+	UFUNCTION ()
+	void OnButton1Clicked ();
+
+	UFUNCTION ()
+	void OnButton2Clicked ();
+
+	UFUNCTION ()
+	void OnButton3Clicked ();
+
+	UFUNCTION ()
+	bool IsQuitGame ( FName LevelName );
+
+	void OpenSelectedLevel ( FName LevelName );
+};
