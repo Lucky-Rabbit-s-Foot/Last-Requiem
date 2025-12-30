c35f659b1427a0a9970d74403a6b3d8532ecb09a
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-30 23:47:44 +0900

    Feat: 저작권 표시 데이터 추가

---
+++ Source/LastRequiem/Public/PJB/Data/P_CopyrightData.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+
+#include "P_CopyrightData.generated.h"
+
+USTRUCT ( BlueprintType )
+struct FCopyrightInfo
+{
+	GENERATED_BODY ()
+
+public:
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly )
+	FString AssetName;
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly )
+	FString License;
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly )
+	FString Author;
+};
+
+UCLASS ()
+class LASTREQUIEM_API UP_CopyrightData : public UDataAsset
+{
+	GENERATED_BODY ()
+
+public:
+	UPROPERTY ( EditAnywhere , BlueprintReadOnly , Category = "Data" )
+	TArray<FCopyrightInfo> CopyrightList;
+};
