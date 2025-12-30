5cea9e577ffc0faed4543c0b8c025b6a44bf3ce3
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-12-30 18:00:21 +0900

    Feat: 발견된 적 구조물 위젯 소스코드 추가

---
+++ Source/LastRequiem/Private/PJB/Obstacle/P_DetectedObstacleWidget.cpp
@@ -0,0 +1,28 @@
+﻿#include "PJB/Obstacle/P_DetectedObstacleWidget.h"
+
+void UP_DetectedObstacleWidget::NativeConstruct ()
+{
+	Super::NativeConstruct ();
+
+	SetVisibility ( ESlateVisibility::Hidden );
+}
+
+void UP_DetectedObstacleWidget::PlayShowAnimation ()
+{
+	if (!AppearAnim)
+	{
+		SetVisibility ( ESlateVisibility::Visible );
+		return;
+	}
+	PlayAnimation ( AppearAnim , 0.0f , 1 , EUMGSequencePlayMode::Forward , 1.0f );
+}
+
+void UP_DetectedObstacleWidget::PlayHideAnimation ()
+{
+	if (!AppearAnim)
+	{
+		SetVisibility ( ESlateVisibility::Hidden );
+		return;
+	}
+	PlayAnimation ( AppearAnim , 0.0f , 1 , EUMGSequencePlayMode::Reverse , 1.0f );
+}
---
+++ Source/LastRequiem/Public/PJB/Obstacle/P_DetectedObstacleWidget.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "P_DetectedObstacleWidget.generated.h"
+
+UCLASS()
+class LASTREQUIEM_API UP_DetectedObstacleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void PlayShowAnimation ();
+	void PlayHideAnimation ();
+
+protected:
+	virtual void NativeConstruct () override;
+
+protected:
+	UPROPERTY ( Transient , meta = (BindWidgetAnim) )
+	class UWidgetAnimation* AppearAnim;
+};
